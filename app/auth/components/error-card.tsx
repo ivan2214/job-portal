77d31a3a7263f1801861e04a28e80bbfbef42a91
ivan2214 <bongiovanniivan12@gmail.com>
2024-12-04 12:50:43 -0300

    fix: :bug: fix login error role user

--- app/auth/components/error-card.tsx
+++ app/auth/components/error-card.tsx
@@ -1,12 +1,12 @@
 import { CardWrapper } from "@/app/auth/components/card-wrapper";
 import { Triangle } from "lucide-react";
 
-export const ErrorCard = () => {
+export const ErrorCard = ({ error }: { error?: string }) => {
 	return (
 		<CardWrapper
 			backButtonHref="/auth/login"
 			backButtonLabel="Back to Login"
-			headderLabel="Oops! Something went wrong"
+			headderLabel={error || "Oops! Something went wrong"}
 		>
 			<div className="flex w-full items-center justify-center">
 				<Triangle className="text-destructive" />
